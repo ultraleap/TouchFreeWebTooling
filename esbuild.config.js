ultraleap/TouchFree

Replace webpack with esbuild

TF_Tooling_Web/esbuild.config.js
@@ -1,0 +1,7 @@
+require('esbuild').build({
+    entryPoints: ['src/TouchFree_Tooling.js', 'src/Plugins/SnappingPlugin/SnappingPlugin.ts'],
+    bundle: true,
+    minify: false,
+    sourcemap: true,
+    outdir: 'dist',
+  });

ultraleap/TouchFreeWebTooling

esbuild.config.js
@@ -1,0 +1,7 @@
+require('esbuild').build({
+    entryPoints: ['src/TouchFree_Tooling.js', 'Plugins/SnappingPlugin/src/SnappingPlugin.ts'],
+    bundle: true,
+    minify: false,
+    sourcemap: true,
+    outdir: 'dist',
+  });