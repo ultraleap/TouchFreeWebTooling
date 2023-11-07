9e980c9573aedac7b1b52ba73ba69f0d89cfee1c
Tom Cartwright <tom.cartwright@ultraleap.com>
2023-11-07 15:10:59 +0000

    Update snapping example for 2.0.0

--- Plugins/SnappingPlugin/Snapping_Example.js
+++ Plugins/SnappingPlugin/Snapping_Example.js
@@ -1,10 +1,11 @@
 window.onload = function () {
     touchfree.init();
+    console.log(touchfree);
     var plugins = [new SnappingPlugin.SnappingPlugin()];
 
-    touchfree.InputActionManager.setPlugins(plugins);
+    touchfree.internal.InputActionManager.setPlugins(plugins);
 
-    touchfree.InputActionManager.plugins[0].setSnapModeToMagnet();
-    touchfree.InputActionManager.plugins[0].setSnapDistance(25);
-    touchfree.InputActionManager.plugins[0].setSnapSoftness(0.3);
+    touchfree.internal.InputActionManager.plugins[0].setSnapModeToMagnet();
+    touchfree.internal.InputActionManager.plugins[0].setSnapDistance(25);
+    touchfree.internal.InputActionManager.plugins[0].setSnapSoftness(0.3);
 };
